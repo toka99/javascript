15f9a62d9d562ac96d1af61c642684eb608b290c
toka99 <56893386+toka99@users.noreply.github.com>
2021-01-08 17:42:48 +0200

    Add files via upload
    day1

---
+++ task1.js
@@ -0,0 +1,6 @@
+
+var fname=prompt("please enter your firstname");
+var lname=prompt("please enter your lastname");
+var year=prompt("please enter your birth year");
+var age=2021-(Number(year));
+alert(`welcome ${fname} ${lname} your age is ${age}`)
---
+++ task2.js
@@ -0,0 +1,6 @@
+
+var result=confirm("it's the first release of a culculator that only has a summation feature");
+var num1=prompt("enter first number");
+var num2=prompt("enter second number");
+var sum=Number(num1)+Number(num2);
+alert(`${num1} + ${num2} = ${sum}`)
